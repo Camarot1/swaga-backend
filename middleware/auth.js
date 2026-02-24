ed0f138e3f455d9ef0a8947037533e033e2109fb
Camarot1 <tupojdoter@gmail.com>
2026-02-24 09:57:35 +0300

    fix auth key

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 module.export = (req, res, next) => {
     const apiKey = req.headers['x-api-key']
 
-    const validKey = process.env.ADMIN_KEY
+    const validKey = process.env.AUTH_KEY
 
     if (!apiKey || apiKey !== validKey){
         return res.status(403).json({
